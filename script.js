f540a80a4adae7678b7905ebc26bb910977be343
rizwanishaq <rizwanishaq@gmail.com>
2019-10-11 12:41:57 +0200

    Updates

--- script.js
+++ script.js
@@ -1,37 +1,32 @@
-const video = document.getElementById('video');
+const video = document.getElementById('video')
 model_url = 'https://cdn.jsdelivr.net/gh/rizwanishaq/facedetection/dist/models/'
 
 Promise.all([
-    faceapi.nets.tinyFaceDetector.loadFromUri(model_url),
-    faceapi.nets.faceLandmark68Net.loadFromUri(model_url),
-    faceapi.nets.faceRecognitionNet.loadFromUri(model_url),
-    faceapi.nets.faceExpressionNet.loadFromUri(model_url),
+  faceapi.nets.tinyFaceDetector.loadFromUri(model_url),
+  faceapi.nets.faceLandmark68Net.loadFromUri(model_url),
+  faceapi.nets.faceRecognitionNet.loadFromUri(model_url),
+  faceapi.nets.faceExpressionNet.loadFromUri(model_url)
 ]).then(startVideo)
 
 function startVideo() {
-    navigator.getUserMedia({
-            video: {}
-        },
-        stream => video.srcObject = stream,
-        err => console.error(err)
-    )
+  navigator.getUserMedia(
+    { video: {} },
+    stream => video.srcObject = stream,
+    err => console.error(err)
+  )
 }
 
-
 video.addEventListener('play', () => {
-    const canvas = faceapi.createCanvasFromMedia(video)
-    document.body.append(canvas)
-    const displaySize = {
-        width: video.width,
-        height: video.height
-    }
-    faceapi.matchDimensions(canvas, displaySize)
-    setInterval(async () => {
-        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
-        const resizeDetections = faceapi.resizeResults(detections, displaySize)
-        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-        faceapi.draw.drawDetections(canvas, resizeDetections)
-        faceapi.draw.drawFaceLandmarks(canvas, resizeDetections)
-        faceapi.draw.drawFaceExpressions(canvas, resizeDetections)
-    }, 100)
+  const canvas = faceapi.createCanvasFromMedia(video)
+  document.body.append(canvas)
+  const displaySize = { width: video.width, height: video.height }
+  faceapi.matchDimensions(canvas, displaySize)
+  setInterval(async () => {
+    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+    const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+    faceapi.draw.drawDetections(canvas, resizedDetections)
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+  }, 100)
 })
